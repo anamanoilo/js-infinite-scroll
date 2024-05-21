12c941f2f52ad6c338426a622bcf172116a58eed
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2024-05-21 22:53:29 +0300

    add loader

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import { createPostItemMarkup } from './js/templates/postItemMarkup';
 const refs = {
   input: document.querySelector('input#filter-box'),
   postList: document.querySelector('.post-list'),
+  loader: document.querySelector('.loader'),
 };
 
 const DEBOUNCE_DELAY = 300;
 let page = 1;
 let query = null;
+
+refs.input.addEventListener('input', debounce(handleFilterPostsInput, DEBOUNCE_DELAY));
+
+loadPage({ page });
 
 const lastPostObserver = new IntersectionObserver(
   ([entry], observer) => {
@@ -24,26 +29,8 @@ const lastPostObserver = new IntersectionObserver(
   },
 );
 
-refs.input.addEventListener('input', debounce(handleFilterPostsInput, DEBOUNCE_DELAY));
-
-function handleFilterPostsInput(e) {
-  query = e.target.value.trim();
-  page = 1;
-  refs.postList.innerHTML = '';
-  loadPage({ page, query });
-}
-
-
-function renderList(data) {
-  const markup = data
-    .map(({ id, title, body }) => {
-      return createPostItemMarkup({ id, title, body });
-    })
-    .join('');
-  refs.postList.insertAdjacentHTML('beforeend', markup);
-}
-
 function loadPage({ page, query }) {
+  showLoader();
   fetchPosts({ page, query })
     .then(data => {
       if (data.length) {
@@ -54,7 +41,35 @@ function loadPage({ page, query }) {
         }
       }
     })
-    .catch(console.log);
+    .catch(console.log)
+    .finally(hideLoader);
 }
 
-loadPage({ page });
+function handleFilterPostsInput(e) {
+  const newQuery = e.target.value.trim();
+  if (query === newQuery) {
+   return;
+  } 
+  query = newQuery;
+  page = 1;
+  refs.postList.innerHTML = '';
+  loadPage({ page, query });
+}
+
+function renderList(data) {
+  const markup = data
+    .map(({ id, title, body }) => {
+      return createPostItemMarkup({ id, title, body });
+    })
+    .join('');
+  refs.postList.insertAdjacentHTML('beforeend', markup);
+}
+
+function hideLoader() {
+  refs.loader.classList.add('is-hidden');
+}
+function showLoader() {
+  refs.loader.classList.remove('is-hidden');
+}
+
+
